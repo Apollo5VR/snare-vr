6edede133ff51a51836827385e73c7e4889a5f29
Graham <graham.gettel@gmail.com>
2021-04-24 13:07:17 -0600

    adding door manager for first scene level progression

---
+++ Assets/Scripts/DoorManager.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DoorManager : MonoBehaviour
+{
+    public ProgressionController progressionController;
+
+    void OnTriggerEnter(Collider other)
+    {
+        Debug.Log("Scene 1 Door collided with:" + other.name + "tag is: " + other.tag);
+
+        if(other.tag == "Player")
+        {
+            //TODO call scene progress on sceneManager
+            progressionController.LoadNextScene();
+        }
+    }
+}
