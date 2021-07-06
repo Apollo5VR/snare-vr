014d8719eed2d4923e1c440f3b522369d873d195
Graham <graham.gettel@gmail.com>
2021-07-05 20:30:11 -0600

    adding hatcontroller for scene progression / hat actions

---
+++ Assets/Scripts/HatController.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HatController : MonoBehaviour
+{
+    public ProgressionController progressionController;
+    public bool isAnimTrigger;
+
+    private void Start()
+    {
+        progressionController = GameObject.Find("ProgressionController").GetComponent<ProgressionController>();
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            Debug.Log("hat triggered by: " + other.tag);
+
+            if (isAnimTrigger)
+            {
+                this.GetComponent<Animator>().enabled = false;
+            }
+            else
+            {
+                progressionController.LoadNextScene();
+            }
+        }
+    }
+}
