5fa3b72a6a81c32b9077702239c2d38976226669
Graham <graham.gettel@gmail.com>
2021-07-11 22:56:57 -0600

    adding pointer arrow functionality

--- Assets/Scripts/HatController.cs
+++ Assets/Scripts/HatController.cs
@@ -6,10 +6,20 @@ public class HatController : MonoBehaviour
 {
     public ProgressionController progressionController;
     public bool isAnimTrigger;
+    public GameObject pointerArrow;
+    // animate the game object from -1 to +1 and back
+    private float minimum = -2.0f;
+    private float maximum = -1.5f;
+
+    // starting value for the Lerp
+    static float t = 0.0f;
 
     private void Start()
     {
         progressionController = GameObject.Find("ProgressionController").GetComponent<ProgressionController>();
+
+        minimum = pointerArrow.transform.position.y - 0.25f;
+        maximum = pointerArrow.transform.position.y + 0.25f;
     }
 
     private void OnTriggerEnter(Collider other)
@@ -21,11 +31,35 @@ public class HatController : MonoBehaviour
             if (isAnimTrigger)
             {
                 this.GetComponent<Animator>().enabled = false;
+                pointerArrow.SetActive(false);
             }
             else
             {
                 progressionController.LoadNextScene();
             }
         }
+    }
+
+    void Update()
+    {
+        if (pointerArrow.activeSelf)
+        {
+            // animate the position of the game object...
+            pointerArrow.transform.position = new Vector3(pointerArrow.transform.position.x, Mathf.Lerp(minimum, maximum, t), pointerArrow.transform.position.z);
+
+            // .. and increase the t interpolater
+            t += 1.5f * Time.deltaTime;
+
+            // now check if the interpolator has reached 1.0
+            // and swap maximum and minimum so game object moves
+            // in the opposite direction.
+            if (t > 1.0f)
+            {
+                float temp = maximum;
+                maximum = minimum;
+                minimum = temp;
+                t = 0.0f;
+            }
+        }   
     }
 }
