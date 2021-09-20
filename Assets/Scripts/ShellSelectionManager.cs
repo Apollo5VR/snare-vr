e70986bc8794d86d1d05750e74f7982f688644e5
Graham <graham.gettel@gmail.com>
2021-09-19 19:34:14 -0600

    shell selection up anim 2

--- Assets/Scripts/ShellSelectionManager.cs
+++ Assets/Scripts/ShellSelectionManager.cs
@@ -12,16 +12,66 @@ public class ShellSelectionManager : MonoBehaviour
     public static Action<GameObject> OnShellSelected;
     public static Action<GameObject> OnBallStopped;
 
+    private float minimum = -2.0f;
+    private float maximum = -1.5f;
+    // starting value for the Lerp
+    static float t = 0.0f;
+
+    public bool testBool;
+
     public void Start()
     {
         OnShellSelected += UpdateShellSelected;
         OnBallStopped += UpdateBallLocation;
     }
 
+    //TODO - remove
+    public void Update()
+    {
+        if (testBool)
+        {
+            StartCoroutine(LiftShell(shellSelected));
+            testBool = false;
+        }
+    }
+
     //Observer in an Observer Pattern
     private void UpdateShellSelected(GameObject shell)
     {
-         shellSelected = shell;
+        shellSelected = shell;
+        StartCoroutine(LiftShell(shellSelected));
+    }
+
+    private IEnumerator LiftShell(GameObject shell)
+    {
+        bool liftCup = true;
+        Vector3 originalPosition = shell.transform.position;
+        maximum = originalPosition.y + 0.75f;
+        minimum = originalPosition.y;
+
+        while (liftCup)
+        {
+            shell.transform.position = new Vector3(shell.transform.position.x, Mathf.Lerp(minimum, maximum, t), shell.transform.position.z);
+
+            // .. and increase the t interpolater
+            t += 1.5f * Time.deltaTime;
+
+            // now check if the interpolator has reached 1.0
+            // and swap maximum and minimum so game object moves
+            // in the opposite direction.
+            if (t > 1.0f)
+            {
+                float temp = maximum;
+                maximum = minimum;
+                minimum = temp;
+                t = 0.0f;
+
+                liftCup = false;
+                shell.transform.position = originalPosition;
+            }
+
+            yield return null;
+        }
     }
 
     private void UpdateBallLocation(GameObject shell)
