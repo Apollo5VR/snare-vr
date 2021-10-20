dd15661ad9ab1ca0315363d186302551da7e6859
Graham <graham.gettel@gmail.com>
2021-10-19 21:28:03 -0600

    update so spellbook requires delay hold to select spell

--- Assets/Scripts/TriggerSpellSelection.cs
+++ Assets/Scripts/TriggerSpellSelection.cs
@@ -4,12 +4,43 @@ using UnityEngine;
 
 public class TriggerSpellSelection : MonoBehaviour
 {
-    private void OnTriggerEnter(Collider other)
+    public GameObject loadingLine;
+    public float touchTime = 0;
+
+    private void OnTriggerStay(Collider other)
     {
         if(other.gameObject.name == "Wand")
         {
             //"Subject" 
-            SpellSelectionWheelManager.OnSpellSelected?.Invoke(this.gameObject);
+            if(touchTime < 1)
+            {
+                loadingLine.transform.localScale = new Vector3(touchTime, loadingLine.transform.localScale.y, loadingLine.transform.localScale.z);
+                touchTime += Time.deltaTime;
+            }
+            else
+            {
+                touchTime = 0;
+                SpellSelectionWheelManager.OnSpellSelected?.Invoke(this.gameObject);
+            }
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        touchTime = 0;
+    }
+
+    private void OnDisable()
+    {
+        touchTime = 0;
+    }
+
+    private void Update()
+    {
+        if (touchTime < 1)
+        {
+            loadingLine.transform.localScale = new Vector3(touchTime, loadingLine.transform.localScale.y, loadingLine.transform.localScale.z);
+            touchTime += Time.deltaTime;
         }
     }
 }
