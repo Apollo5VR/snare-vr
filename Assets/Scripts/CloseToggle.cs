1b8334d0b5716527be79817d99dd7258c0d08e41
Graham <graham.gettel@gmail.com>
2021-10-26 09:35:04 -0600

    text updates & lvl 9 fix

--- Assets/Scripts/CloseToggle.cs
+++ Assets/Scripts/CloseToggle.cs
@@ -5,23 +5,27 @@ using UnityEngine.UI;
 
 public class CloseToggle : MonoBehaviour
 {
-    public Text[] texts;
+    public GameObject[] texts;
     public float timePassed;
     public AudioSource teaserAudio;
 
     // Update is called once per frame
     void Update()
     {
-        if(timePassed > 8)
+        if(timePassed > 8 && timePassed < 9)
         {
             //deactivate text
-            texts[0].enabled = false;
-            texts[1].enabled = true;
+            texts[0].SetActive(false);
+            texts[1].SetActive(true);
 
             teaserAudio.Play();
+
+            timePassed = 9;
         }
-        if(timePassed > 40)
+        if(timePassed > 60 && timePassed < 61)
         {
+            timePassed = 61;
+
             //close game
             Application.Quit();
         }
