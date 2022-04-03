46396de017f33cd4aac3ba3022355324416a9e98
Graham <graham.gettel@gmail.com>
2022-04-03 15:16:20 -0600

    further adds

---
+++ Assets/SnareVR/WireController.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WireController : MonoBehaviour
+{
+    //refernce to wire render array
+    //rerence to wire joint array
+    //state or state bool - modified by triggers
+    //reference to the half / fully completed models (and colliders)
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/SnareVR/WireTrigger.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WireTrigger : MonoBehaviour
+{
+    private void OnTriggerEnter()
+    {
+        
+    }
+    
+}
