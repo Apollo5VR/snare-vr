57412fcdeedfa8e50a7c4de7e09eca6e07561dbe
Graham <graham.gettel@gmail.com>
2022-09-08 17:22:01 -0600

    setting up addressable system with UGS CCD

---
+++ Assets/Scripts/SnareVR/Addressable System/AddressablesManager.cs
@@ -0,0 +1,41 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AddressableAssets;
+using UnityEngine.ResourceManagement.AsyncOperations;
+
+public class AddressablesManager : MonoBehaviour
+{
+    [SerializeField]
+    private Transform[] m_TrophyAnchors;
+
+    private AsyncOperationHandle m_TrophyLoadingHandle;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        SetTrophies();
+    }
+
+    private void SetTrophies()
+    {
+        m_TrophyLoadingHandle = Addressables.InstantiateAsync("deer", m_TrophyAnchors[0]);//bearhead
+
+        m_TrophyLoadingHandle.Completed += OnTrophiesInstantiated;
+    }
+
+    private void OnTrophiesInstantiated(AsyncOperationHandle obj)
+    {
+        if(obj.Status == AsyncOperationStatus.Succeeded)
+        {
+            Debug.Log("Trophies have successfully loaded");
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
