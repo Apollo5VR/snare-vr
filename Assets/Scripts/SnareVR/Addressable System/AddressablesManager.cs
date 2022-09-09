8ec22d1a0ad37abfb9d59c20c278865338ab3c6e
Graham <graham.gettel@gmail.com>
2022-09-09 17:59:27 -0600

    updates to autosize trophy to mount head scale & notes for combining Inventory w/ CCD addressables

--- Assets/Scripts/SnareVR/Addressable System/AddressablesManager.cs
+++ Assets/Scripts/SnareVR/Addressable System/AddressablesManager.cs
@@ -12,6 +12,9 @@ public class AddressablesManager : MonoBehaviour
 
     private AsyncOperationHandle m_TrophyLoadingHandle;
 
+    public BoxCollider boxCollider;
+    public GameObject bear;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -20,9 +23,19 @@ public class AddressablesManager : MonoBehaviour
 
     private void SetTrophies()
     {
-        m_TrophyLoadingHandle = Addressables.InstantiateAsync("bearhead", m_TrophyAnchors[0]);//bearhead
+        //TODO - pull the handle, creating array of all CCD items
+        //then pull inventory items. if has type of "trophy" then add it to the populating array
+        //these inventory items will match the exact names of the CCD items, allowing to determine "if has inventory item, spawn CCD item of same name"
+        //then simply load them up 1 - 4 on the trophy stands via those stored transform locations (always the same)
+        //Sizing? Handled by below (means new object has to be confined within the 1:1:1 box collider format we use for trophies
 
-        m_TrophyLoadingHandle.Completed += OnTrophiesInstantiated;
+        Collider collider = bear.GetComponent<BoxCollider>();
+        GameObject bearGo = Instantiate(bear, boxCollider.transform);
+        bearGo.transform.localScale = new Vector3(boxCollider.bounds.size.x / collider.bounds.size.x, boxCollider.bounds.size.y / collider.bounds.size.y, boxCollider.bounds.size.z / collider.bounds.size.z);  //change to actual adjustment
+
+        //m_TrophyLoadingHandle = Addressables.InstantiateAsync("bearhead", m_TrophyAnchors[0]);//bearhead
+
+        //m_TrophyLoadingHandle.Completed += OnTrophiesInstantiated;
     }
 
     private void OnTrophiesInstantiated(AsyncOperationHandle obj)
@@ -41,7 +54,7 @@ public class AddressablesManager : MonoBehaviour
 
     private void OnDestroy()
     {
-        m_TrophyLoadingHandle.Completed -= OnTrophiesInstantiated;
-        Addressables.Release(m_TrophyLoadingHandle);
+        //m_TrophyLoadingHandle.Completed -= OnTrophiesInstantiated;
+        //Addressables.Release(m_TrophyLoadingHandle);
     }
 }
