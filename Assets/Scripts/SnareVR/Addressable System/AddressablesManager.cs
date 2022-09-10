2506767e37899a5816e70c9d92b4fe42e6b4a76b
Graham <graham.gettel@gmail.com>
2022-09-09 18:23:21 -0600

    adjusting item names for dynamic functionality (via generic labels)

--- Assets/Scripts/SnareVR/Addressable System/AddressablesManager.cs
+++ Assets/Scripts/SnareVR/Addressable System/AddressablesManager.cs
@@ -29,13 +29,13 @@ public class AddressablesManager : MonoBehaviour
         //then simply load them up 1 - 4 on the trophy stands via those stored transform locations (always the same)
         //Sizing? Handled by below (means new object has to be confined within the 1:1:1 box collider format we use for trophies
 
-        Collider collider = bear.GetComponent<BoxCollider>();
-        GameObject bearGo = Instantiate(bear, boxCollider.transform);
-        bearGo.transform.localScale = new Vector3(boxCollider.bounds.size.x / collider.bounds.size.x, boxCollider.bounds.size.y / collider.bounds.size.y, boxCollider.bounds.size.z / collider.bounds.size.z);  //change to actual adjustment
+        //Collider collider = bear.GetComponent<BoxCollider>();
+        //GameObject bearGo = Instantiate(bear, boxCollider.transform);
+        //bearGo.transform.localScale = new Vector3(boxCollider.bounds.size.x / collider.bounds.size.x, boxCollider.bounds.size.y / collider.bounds.size.y, boxCollider.bounds.size.z / collider.bounds.size.z);  //change to actual adjustment
 
-        //m_TrophyLoadingHandle = Addressables.InstantiateAsync("bearhead", m_TrophyAnchors[0]);//bearhead
+        m_TrophyLoadingHandle = Addressables.InstantiateAsync("trophy1", m_TrophyAnchors[0]);//bearhead
 
-        //m_TrophyLoadingHandle.Completed += OnTrophiesInstantiated;
+        m_TrophyLoadingHandle.Completed += OnTrophiesInstantiated;
     }
 
     private void OnTrophiesInstantiated(AsyncOperationHandle obj)
@@ -54,7 +54,7 @@ public class AddressablesManager : MonoBehaviour
 
     private void OnDestroy()
     {
-        //m_TrophyLoadingHandle.Completed -= OnTrophiesInstantiated;
-        //Addressables.Release(m_TrophyLoadingHandle);
+        m_TrophyLoadingHandle.Completed -= OnTrophiesInstantiated;
+        Addressables.Release(m_TrophyLoadingHandle);
     }
 }
